62899d51c89acfcf68a322637008999536d7298b
Rhys Gevaux <rhys-github@sandimas.co>
2021-10-26 20:16:39 +1100

    minimum, maximum - require non empty list

--- src/maximum.ts
+++ src/maximum.ts
@@ -1,8 +1,6 @@
-import { isNotEmpty } from "./is-not-empty";
 import { max } from "./max";
-import { List } from "./types";
+import { NonEmptyList } from "./types";
 
-export function maximuum<N extends number>(numbers: List<N>): N {
-  if (!isNotEmpty(numbers)) throw new Error("empty list");
+export function maximuum<N extends number>(numbers: NonEmptyList<N>): N {
   return max(...numbers);
 }
--- src/minimum.ts
+++ src/minimum.ts
@@ -1,8 +1,6 @@
-import { isNotEmpty } from "./is-not-empty";
 import { min } from "./min";
-import { List } from "./types";
+import { NonEmptyList } from "./types";
 
-export function minimum<N extends number>(numbers: List<N>): N {
-  if (!isNotEmpty(numbers)) throw new Error("empty list");
+export function minimum<N extends number>(numbers: NonEmptyList<N>): N {
   return min(...numbers);
 }
