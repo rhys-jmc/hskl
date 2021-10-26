ecffd4fabfb600c22e5240528d1b94beb151d185
Rhys Gevaux <rhys-github@sandimas.co>
2021-10-26 22:52:55 +1100

    range from char

--- src/range.test.ts
+++ src/range.test.ts
@@ -4,4 +4,9 @@ describe("range", () => {
   test("number", () => {
     expect(range(1, 20)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]);
   });
+
+  test("char", () => {
+    expect(range("a", "z")).toBe("abcdefghijklmnopqrstuvwxyz");
+    expect(range("K", "Z")).toBe("KLMNOPQRSTUVWXYZ");
+  });
 });
--- src/range.ts
+++ src/range.ts
@@ -1,7 +1,26 @@
 import { List } from "./types";
 
-export function range(min: number, max: number): List<number> {
+export function range(min: string, max: string): string;
+export function range(min: number, max: number): List<number>;
+export function range<T>(min: number | string, max: number | string): List<number> | string {
+  if (typeof min === "number" || typeof max === "number") {
+    if (typeof min !== "number" || typeof max !== "number") throw new Error("min and max must be same type");
+    return generateRange(min, max);
+  }
+
+  if (min.length !== 1 || max.length !== 1) throw new Error("min and max string must be single char");
+
+  const minChar = min[0] as string;
+  const maxChar = max[0] as string;
+  const minCode = minChar.charCodeAt(0);
+  const maxCode = maxChar.charCodeAt(0);
+  const charCodes = generateRange(minCode, maxCode);
+
+  return Array.from(charCodes, (code) => String.fromCharCode(code)).join("");
+}
+
+const generateRange = (min: number, max: number) => {
   const length = max - min + 1;
   if (length < 0) throw new Error("max must be larger than min");
   return Array.from({ length }, (_, index) => min + index);
-}
+};
