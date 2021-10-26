d338815795aea892a0994a590db7331f4516674d
Rhys Gevaux <rhys-github@sandimas.co>
2021-10-26 20:19:58 +1100

    nit: reverse.ts file order

--- src/reverse.ts
+++ src/reverse.ts
@@ -1,8 +1,8 @@
 import { List, NonEmptyList, Take } from "./types";
 
-type Reverse<L extends List> = L extends NonEmptyList<infer T, infer R> ? Append<T, Reverse<R>> : L;
 export function reverse<L extends List>(array: L): Take<22, L> extends L ? Reverse<L> : L[number][] {
   return [...array].reverse() as any;
 }
 
 type Append<A, L extends List> = readonly [...L, A];
+type Reverse<L extends List> = L extends NonEmptyList<infer T, infer R> ? Append<T, Reverse<R>> : L;
