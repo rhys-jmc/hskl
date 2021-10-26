61fd1bb1dc3f32175cb17745b84baee11e55b6b9
Rhys Gevaux <rhys-github@sandimas.co>
2021-10-26 20:41:33 +1100

    Test maximum

---
+++ src/maximum.test.ts
@@ -0,0 +1,5 @@
+import { maximum } from "./maximum";
+
+test("maximum", () => {
+  expect(maximum([1, 9, 2, 3, 4])).toBe(9);
+});
--- src/maximum.ts
+++ src/maximum.ts
@@ -1,6 +1,6 @@
 import { max } from "./max";
 import { NonEmptyList } from "./types";
 
-export function maximuum<N extends number>(numbers: NonEmptyList<N>): N {
+export function maximum<N extends number>(numbers: NonEmptyList<N>): N {
   return max(...numbers);
 }
