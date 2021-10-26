f8c512523c9d3d0ea580787d2b9a4d8496163d15
Rhys Gevaux <rhys-github@sandimas.co>
2021-10-26 22:08:26 +1100

    Use cycle with take

--- src/take.test.ts
+++ src/take.test.ts
@@ -1,8 +1,16 @@
+import { cycle } from "./cycle";
 import { take } from "./take";
 
-test("take", () => {
-  expect(take(3, [5, 4, 3, 2, 1])).toEqual([5, 4, 3]);
-  expect(take(1, [3, 9, 3])).toEqual([3]);
-  expect(take(5, [1, 2])).toEqual([1, 2]);
-  expect(take(0, [6, 6, 6])).toEqual([]);
+describe("take", () => {
+  test("list", () => {
+    expect(take(3, [5, 4, 3, 2, 1] as const)).toEqual([5, 4, 3]);
+    expect(take(1, [3, 9, 3] as const)).toEqual([3]);
+    expect(take(5, [1, 2])).toEqual([1, 2]);
+    expect(take(0, [6, 6, 6] as const)).toEqual([]);
+  });
+
+  test("cycle", () => {
+    expect(take(10, cycle([1, 2, 3] as const))).toEqual([1, 2, 3, 1, 2, 3, 1, 2, 3, 1]);
+    expect(take(12, cycle("LOL "))).toBe("LOL LOL LOL ");
+  });
 });
--- src/take.ts
+++ src/take.ts
@@ -1,5 +1,13 @@
 import { List, Take } from "./types";
 
-export function take<E extends number, L extends List>(extract: E, list: L): Take<E, L> {
-  return list.slice(0, extract) as Take<E, L>;
+export function take(extract: number, fn: (extract: number) => string): string;
+export function take<T, L extends List<T>>(extract: number, fn: (extract: number) => List<T>): List<T>;
+export function take<T, E extends number, L extends List<T>>(extract: E, list: L): Take<E, L>;
+export function take<T, E extends number, L extends List<T> | string>(
+  extract: E,
+  list: L | ((extract: number) => L | string)
+): typeof list extends Function ? L : Take<E, L> {
+  return (
+    typeof list === "function" ? list(extract) : (list.slice(0, extract) as Take<E, L>)
+  ) as typeof list extends Function ? L : Take<E, L>;
 }
